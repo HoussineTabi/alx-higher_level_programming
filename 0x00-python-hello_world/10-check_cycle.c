39b23d1da3df90ce51980be9149942bd790f2231
HoussineTabi <0620036304ht@gmail.com>
2023-08-07 14:44:23 +0000

    task8

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "list.h"
+
+/**
+ * check_cycle - checks if there is a cycle
+ * @list: the list to check
+ *
+ * Return: 0 if there is no cycle and 1 if there is one
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *previous = list;
+
+	while (list)
+	{
+		list = list->next;
+		if (list > previouse)
+			return (1);
+		previouse = list;
+	}
+	return (0);
+}
