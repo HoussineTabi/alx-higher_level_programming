95fc8af9ca52f1ba754699e9c4a2515e6267cef7
HoussineTabi <0620036304ht@gmail.com>
2023-08-08 14:30:59 +0000

    tasks

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str1 = ''
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        str1 += chr(65 + i - ord("a"))
+    else:
+        str1 += chr(i)
+print("{}".format(str1), end='')
