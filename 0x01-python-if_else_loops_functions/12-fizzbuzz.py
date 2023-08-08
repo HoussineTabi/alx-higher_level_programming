cf94158c1f9b5e65ae07cc30cdaa1ca9b0a9f223
HoussineTabi <0620036304ht@gmail.com>
2023-08-08 12:48:29 +0000

    tasks

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,7 +7,7 @@ def fizzbuzz():
             if i != 100:
                 print("{}".format("Fizz"), end=' ')
             else:
-                print("{}".format("Buzz"))
+                print("{}".format("Buzz"), end=' ')
         elif i % 3 == 0:
             print("{}".format("Fizz"), end=' ')
         else:
