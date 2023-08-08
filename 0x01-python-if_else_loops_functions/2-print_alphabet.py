183a0b966ac36b6ba3aaadc1bafb3a43d2e8cd99
HoussineTabi <0620036304ht@gmail.com>
2023-08-08 10:44:05 +0000

    task_to_5

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(len("abcdefghijklmnopqrstuvwxyz")):
-    print("abcdefghijklmnopqrstuvwxyz"[i], sep='', end='')
+    print("{}".format("abcdefghijklmnopqrstuvwxyz"[i]), sep='', end='')
