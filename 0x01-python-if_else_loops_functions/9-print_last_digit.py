d60a1d836b2e46802dbc30576bee7ca572ff3759
HoussineTabi <0620036304ht@gmail.com>
2023-08-08 12:19:18 +0000

    tasks

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
     r = number % 10
     if number < 0:
         r = r - 10
-    print("{}".format(r), end='')
+    print("{}".format(r),sep='', end='')
     return (r)
