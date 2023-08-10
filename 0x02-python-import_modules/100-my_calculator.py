25282b55112f7e889b67bc600f4b48c333acbab6
HoussineTabi <0620036304ht@gmail.com>
2023-08-10 23:59:52 +0000

    taskoptio1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    op = argv[2]
+    if op != '+' or op != '-' or op != '*' or op != '/':
+        print("Unknown operator, Available operators: +, -, * and /")
+    else:
+        a = int(argv[1])
+        b = int(argv[2])
+        if op == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        if op == '-':
+            print("{} + {} = {}".format(a, b, sub(a, b)))
+        if op == '*':
+            print("{} + {} = {}".format(a, b, ,mul(a, b)))
+        if op == '/':
+            print("{} + {} = {}".format(a, b, div(a, b)))
