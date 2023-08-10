981238ee7239f8f4c013e3e0e9045eb37acecbe9
HoussineTabi <0620036304ht@gmail.com>
2023-08-10 18:29:36 +0000

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print("{}"format(0))
+        print("{}"format(len(argv)) - 1)
     else:
         length = 1
         add = 0
