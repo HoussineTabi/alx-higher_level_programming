92ddd85d1a0c8297b96d0dbfde82c65047b89a74
HoussineTabi <0620036304ht@gmail.com>
2023-08-11 10:10:43 +0000

    taskss

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     if len(argv) == 1:
-        print("{} arguments.".format(len(argv) - 1))
+        print("0 arguments")
     elif len(argv) == 2:
         print("1: {}".format(argv[1]))
     else:
