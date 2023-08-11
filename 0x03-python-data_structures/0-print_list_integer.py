2b946fbeb689cac766be1fa725f2f2d38d516021
HoussineTabi <0620036304ht@gmail.com>
2023-08-11 14:23:01 +0000

    first_commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import string
+
+
 def print_list_integer(my_list=[]):
-    """This function prints a list of integer
-       Arguments:
-                 my_list: the list of integer
+    """This function is prints a list of integer
+       Argmuents:
+                 my_list: the list that contains the integes
     """
     for i in my_list:
         print(str.format(i))
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """This function returns an element from a list at
+       index if the item is exist and None otherwise
+       Arguments:
+                 my_list: the list of integers
+                 idx: the index of elemnt
+       Return: the element if exist and None otherwise
+       """
+    if len(my_list) < idx - 1 or idx < 0:
+        return (None)
+    return (my_list[idx])
