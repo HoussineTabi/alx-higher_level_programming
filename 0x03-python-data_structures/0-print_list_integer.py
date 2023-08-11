88c7012e217d591d3657d32c04fa3ed8b8aaf506
HoussineTabi <0620036304ht@gmail.com>
2023-08-11 13:59:50 +0000

    first_commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import string
+def print_list_integer(my_list=[]):
+    """This function prints a list of integer
+       Arguments:
+                 my_list: the list of integer
+    """
+    for i in my_list:
+        print(str.format(i))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+what are list and tuple in python and how to work with them
