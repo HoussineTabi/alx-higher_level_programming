19a2b7369d2d86891e6b0c735492a9241a9f62ae
HoussineTabi <0620036304ht@gmail.com>
2023-08-12 10:13:37 +0000

    corre

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """This function add to tuple together in a new tuple
        Return: the new tuple
     """
-    if (
     sum_element = []
     a = max(len(tuple_a), len(tuple_b))
     for i in range(a):
