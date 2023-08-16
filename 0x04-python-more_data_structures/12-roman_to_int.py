932ee1651831d4c595a2e4fd2364c21e416540f5
HoussineTabi <0620036304ht@gmail.com>
2023-08-16 22:36:26 +0000

    tass12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,24 +2,19 @@
 def roman_to_int(roman_string):
     """This function converts a Roman
        number tu an integer number"""
-    if type(roman_string) is not str or len(roman_string) == 0:
-        return 0
-    if roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
     number = 0
-    roman_dict = {"I": 1, "II": 2, "III": 3, "IV": 4,
-                  "V": 5, "VI": 6, "VII": 7, "VIII": 8,
-                  "IX": 9, "X": 10, "L": 50, "C": 100,
-                  "D": 500, "M": 1000}
+    rm_dt = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     j = 0
     x = None
     for i in roman_string:
         if j == 0:
             x = i
             j = 1
-        if roman_dict[i] > roman_dict[x]:
-            number = (number + roman_dict[i] - 2 * roman_dict[x])
+        if rm_dt[i] > rmn_dt[x]:
+            number = (number + rm_dt[i] - 2 * rm_dt[x])
         else:
-            number += roman_dict[i]
+            number += rm_dt[i]
         x = i
     return number
