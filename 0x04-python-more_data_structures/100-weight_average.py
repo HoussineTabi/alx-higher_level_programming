d0a945c8324f44e83515b34c51c98742c32eed96
HoussineTabi <0620036304ht@gmail.com>
2023-08-16 17:18:56 +0000

    tas100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == None or len(my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return 0
     average = 0
     devided = 0
