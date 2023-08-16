cd5666499ee463de2975f4c8ace64d339acf2732
HoussineTabi <0620036304ht@gmail.com>
2023-08-16 17:18:00 +0000

    tas100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == None or len(my_list) == 0:
+        return 0
+    average = 0
+    devided = 0
+    for (i, j) in my_list:
+        average += i * j
+        devided += j
+    return (average / devided)
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """This function converts a Roman
+       number tu an integer number"""
+    if roman_strint not str:
+        return None
+    number = 0
+    roman_dict = {"I" : 1, "II" : 2, "III" : 3,\
+            "IV" : 4, "V" : 5, "VI" : 6, "VII" : 7, "VIII" : 8,\
+            "IX" : 9, "X" : 10, "L" : 50, "C" : 100,\
+            "D" : 500, "M" : 1000}
+    
