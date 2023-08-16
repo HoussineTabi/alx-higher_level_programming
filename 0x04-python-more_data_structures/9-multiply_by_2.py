26f82c7efe4e8a54e7717b6d90187bdfd92bca22
HoussineTabi <0620036304ht@gmail.com>
2023-08-16 18:08:20 +0000

    tas109

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,7 @@
 def multiply_by_2(a_dictionary):
     new_dict = dict()
     list_keys = [key for key in a_dictionary]
-    for i in list_key:
+    for i in list_keys:
         new_dict[i] = a_dictionary[i] * 2
     return new_dict
+print(multiply_by_2({"hassan" : 2, "hossine" : 4}))
