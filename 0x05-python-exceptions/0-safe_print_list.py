b4f3730801d900f816c18adb7ff1327117c35ca6
HoussineTabi <0620036304ht@gmail.com>
2023-08-28 10:17:25 +0000

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    if x < 0:
-        return (0)
-    for i in range(x):
-        try:
-            print(my_list[i], end='')
-        except IndexError:
-            print()
-            return (i)
+if "__name__" == "__main__":
+    def safe_print_list(my_list=[], x=0):
+        for i in range(x):
+            try:
+                print(my_list[i], end='')
+            except IndexError:
+                print()
+        return (i)
     print()
     return (i + 1)
