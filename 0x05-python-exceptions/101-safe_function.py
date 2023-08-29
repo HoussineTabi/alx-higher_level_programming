283eb5ada2531f73a3d84af18ffbd9782fc124c0
HoussineTabi <0620036304ht@gmail.com>
2023-08-29 07:41:06 +0000

    task_optionaly

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -12,5 +12,18 @@ def safe_function(fct, *arg):
         print("Exception: {}".format(err), file=sys.stderr)
     except ZeroDivisionError as err:
         print("Exception: {}".format(err), file=sys.stderr)
+    except IndexError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+    except NameError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
     finally:
         return a
+def print_list(my_list, len):
+    i = 0
+    while i < len:
+        print(my_list[i])
+        i += 1
+    return len
+
+result = safe_function(print_list, [1, 2, 3, 4], 10)
+print("result of print_list: {}".format(result))
