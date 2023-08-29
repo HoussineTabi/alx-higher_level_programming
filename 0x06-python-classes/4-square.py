97a61220215b2601317339ba263953059682783d
HoussineTabi <0620036304ht@gmail.com>
2023-08-29 17:13:32 +0000

    tasks

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This class is used to Create object of type Square
+"""
+
+
+class Square:
+    """
+    This class is used to define a square object
+    """
+    def __init__(self, size=0):
+        """
+        This mothod initialize an object of the class square
+        """
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        calculate the area of a square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        get the value of the private attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        size is function sets a value in the attribute size
+        """
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be integer")
