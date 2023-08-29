d41ca8479abb65bce03c48d94768483ad3b35ff4
HoussineTabi <0620036304ht@gmail.com>
2023-08-29 10:43:43 +0000

    tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,6 +2,8 @@
 """
 This model to initialize an object of type Square
 """
+
+
 class Square:
     """
     This is the class that define a square
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""
+This class is used to Create object of type Square
+"""
 class Square:
-    """This class is used to define a square object"""
+    """
+    This class is used to define a square object
+    """
+
+
     def __init__(self, size=0):
-        """This mothod initialize an object of the class square"""
+        """
+        This mothod initialize an object of the class square
+        """
         if isinstance(size, int):
             if size >= 0:
                 self.__size = size
