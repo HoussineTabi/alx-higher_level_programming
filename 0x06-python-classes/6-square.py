48687cf403798d08351d7a2e6e78719ba65838d4
HoussineTabi <0620036304ht@gmail.com>
2023-08-29 18:12:31 +0000

    task5

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,9 +59,9 @@ class Square:
         print a square in the stdout with #
         """
         print("\n" * self.position[0])
-        for i in range(self.size):
-            print(" " * self.position[1], "#" * self.size, sep='')
-        if (self.size == 0):
+        for i in range(self.__size):
+            print(" " * self.__position[1], "#" * self.__size, sep='')
+        if (self.__size == 0):
             print()
 
     @property
