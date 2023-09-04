8f750f6efe3ca12459f14cad391da1a7a2da56e5
HoussineTabi <0620036304ht@gmail.com>
2023-09-04 23:14:15 +0000

    task10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -7,8 +7,18 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
-if not isinstance(sys.argv[1], int):
+N = int(sys.argv[1])
+if not isinstance(N, int):
     print("N must be a number")
 else:
-    if sys.argv[1] < 4:
+    if N < 4:
         print("N must be at less 4")
+    else:
+        for i in range(1, N - 1):
+            lst = []
+            for j in range(N):
+                if i + 2 * j < N:
+                    lst.append([j, i + 2 * j])
+                else:
+                    lst.append([j, abs(N - 2 * j)])
+            print(lst)
