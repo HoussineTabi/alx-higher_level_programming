6c20222112b9935bea144d0e515a78c1c79ff168
HoussineTabi <0620036304ht@gmail.com>
2023-09-04 19:50:42 +0000

    task7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -19,7 +19,6 @@ class Rectangle:
         self.height = height
         Rectangle.number_of_instances += 1
         self.print_symbol = Rectangle.print_symbol
-        
 
     @property
     def width(self):
