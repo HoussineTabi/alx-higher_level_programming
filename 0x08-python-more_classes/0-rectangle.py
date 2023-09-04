7a0910aaaef20ca17e6f8393585e97f7a4543bff
HoussineTabi <0620036304ht@gmail.com>
2023-09-04 15:18:13 +0000

    task0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+In this module we create a class to define a Rectangle
+"""
+
+
+class Rectangle:
+    """
+    In this class we dfine a Rectangle
+    """
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(my_rectangle)
+    print(my_rectangle.__dict__)
