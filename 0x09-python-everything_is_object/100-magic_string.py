3f7366d8204ca4966ba8a378cf84338c50215966
HoussineTabi <0620036304ht@gmail.com>
2023-09-05 10:42:48 +0000

    tasks_new_project

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(iterator={"first":0}):
+def magic_string(iterator={"first": 0}):
     iterator["first"] = iterator["first"] + 1
     return "BestSchool" + ", BestSchool" * (iterator["first"] - 1)
