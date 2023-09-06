bd79c2d38a72735c966a5ae2dd95d0448722ce99
HoussineTabi <0620036304ht@gmail.com>
2023-09-06 07:18:47 +0000

    tasks_correc

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,13 +10,15 @@ class testMaxinteger(unittest.TestCase):
     """
     Class to test my code the max integer
     """
+
     def test_regular_max(self):
         """
         test the regular output
         """
         self.assertEqual(Max_integer.max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(Max_integer.max_integer([]), None)
+
+
 if __name__ == "__main__":
     unittest.main()
     print(Max_integer.max_integer([1, 3]))
-
