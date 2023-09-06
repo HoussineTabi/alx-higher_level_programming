0f93c7fccb3eab751b9cd72b134c4f6bbe8fe720
HoussineTabi <0620036304ht@gmail.com>
2023-09-06 07:16:02 +0000

    tasks_correc

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,7 +12,7 @@ def text_indentation(text):
         check = 0
         for i in text:
             if i in ['.', '?', ':']:
-                print(i,'\n', sep='')
+                print(i, '\n', sep='')
                 check = 0
             else:
                 if i == ' ' and check == 0:
@@ -24,14 +24,6 @@ def text_indentation(text):
 
 
 if __name__ == "__main__":
-    #import doctest
-    #doctest.testfile("tests/5-text_indentation.txt")
-    text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
-    Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere?\
-    Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud:\
-    Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere.\
-    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum\
-    rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo\
-    stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum\
-    cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio\
-    beatiorem! Iam ruinas videres""")
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
+    text_indentation("Hello, World!")
