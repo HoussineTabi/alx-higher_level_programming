284101e8f2a60e60b7731d77affa65fb01065639
HoussineTabi <0620036304ht@gmail.com>
2023-09-06 07:37:56 +0000

    tasks_correc

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,12 +11,31 @@ class testMaxinteger(unittest.TestCase):
     Class to test my code the max integer
     """
 
+    def test_docstr_module(self):
+        """
+        test the existance of the module doc
+        """
+        docstr = __import__("max_integer").__doc__
+        self.assertTrue(len(docstr) > 1)
+
+    def test_docstr_func(self):
+        """
+        checking for the docstr function
+        """
+        docstr = __import__("max_integer").max_integer.__doc__
+        self.assertTrue(len(docstr) > 1)
+
+    def test_empty(self):
+        """
+        checking for the empty list
+        """
+        self.assertIsNone(max_integer([]))
+
     def test_regular_max(self):
         """
         test the regular output
         """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([]), None)
 
 
 if __name__ == "__main__":
