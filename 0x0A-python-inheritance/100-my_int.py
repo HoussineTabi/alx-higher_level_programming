4dd2fefe5d4d2b14c36ea0543b0e279df17b18c7
HoussineTabi <0620036304ht@gmail.com>
2023-09-11 21:12:19 +0000

    tasks

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+That class inherits from the int class
+"""
+
+
+class MyInt(int):
+    """
+    class MyInt inherit from the int class
+    """
+    def __int__(self, value):
+        self.value = value
+
+
+if __name__ == "__main__":
+    my_i = MyInt(3)
+    print(my_i)
+    print(my_i == 3)
+    print(my_i != 3)
