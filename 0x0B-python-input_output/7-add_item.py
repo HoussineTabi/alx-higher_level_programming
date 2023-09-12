9decfdde1cc0f747ecd9fe4b0613650a17a87b40
HoussineTabi <0620036304ht@gmail.com>
2023-09-12 23:21:44 +0000

    task5

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,13 @@ the scrinpt to read and write from a json file
 """
 import json
 import sys
-from os import path
 
 filename = "add_item.json"
 
-__import__("6-load_from_json_file").load_from_json_file(filename)
-
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 arguments = sys.argv[1:]
-__import__("5-save_to_json_file").save_to_json_file(arguments, filename)
+save_to_json_file(arguments, filename)
+load_from_json_file(filename)
 if __name__ == "__main__":
     print()
