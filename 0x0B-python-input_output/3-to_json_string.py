9fa2877c92b85332dc9671a3cf75b38318385841
HoussineTabi <0620036304ht@gmail.com>
2023-09-12 22:09:36 +0000

    task3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,10 +2,15 @@
 """
 Module to read json data type
 """
+import json
 
 
 def to_json_string(my_obj):
     """
     Return the json representation of an object
     """
+    return json.dumps(my_obj)
 
+
+if __name__ == "__main__":
+    print()
