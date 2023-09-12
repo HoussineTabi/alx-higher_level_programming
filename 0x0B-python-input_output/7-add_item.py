8edf76dc6d5e854767cc5ef9d83530c6360c39ec
HoussineTabi <0620036304ht@gmail.com>
2023-09-12 23:23:21 +0000

    task5

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-the scrinpt to read and write from a json file
-"""
+"""this place ofcomment that describes my models"""
 import json
 import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-filename = "add_item.json"
 
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 arguments = sys.argv[1:]
-save_to_json_file(arguments, filename)
-load_from_json_file(filename)
-if __name__ == "__main__":
-    print()
+save_to_json_file(arguments, "add_item.json")
+load_from_json_file("add_item.json")
