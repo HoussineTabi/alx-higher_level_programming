befa4a86838dd56a17e56d0c4783cbe449861265
HoussineTabi <0620036304ht@gmail.com>
2023-09-12 23:30:20 +0000

    task5

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,8 +9,8 @@ def class_to_json(obj):
     unction that returns the dictionary
     data structure
     """
-    dict_attributes = vars(obj)
-    return json.dumps(dict_attributes)
+    
+    return "string"
 
 
 if __name__ = "__main__":
