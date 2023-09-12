39c9659d89eca191e242c08531c04790bd246f13
HoussineTabi <0620036304ht@gmail.com>
2023-09-12 22:34:43 +0000

    task5

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+how to load from a file json
+"""
+
+
+def load_from_json_file(filename):
+    """
+    load the data from a file json file
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.loads(f.read())
+
+
+if __name__ == "__main__":
+    print()
