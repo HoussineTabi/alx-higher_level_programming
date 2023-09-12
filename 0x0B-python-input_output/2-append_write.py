04b686a3aa57f15d43f63b8af66311f588fb2d43
HoussineTabi <0620036304ht@gmail.com>
2023-09-12 16:11:13 +0000

    task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,6 +9,7 @@ def append_write(filename="", text=""):
     append_write used to appending in a file
     """
     with open(filename, "a+", encoding="utf-8") as f:
+        f.seek(1,1)
         return f.write(filename, text)
 
 
