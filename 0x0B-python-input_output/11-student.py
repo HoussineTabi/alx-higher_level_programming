ede3aadde2175cd0ebb8a1e106a1ba71a3721bec
HoussineTabi <0620036304ht@gmail.com>
2023-09-13 07:35:44 +0000

    task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+the module of the task number 10
+"""
+
+
+class Student:
+    """
+    This class represent the student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            rdic = self.__dict__.items()
+            return {k: v for k, v in rdic if k in attrs}
+
+    def reload_from_json(self, json):
+        """
+        reload the attribut from json
+        """
+        self.__dict__ = json
+
+
+if __name__ == "__main__":
+    print()
