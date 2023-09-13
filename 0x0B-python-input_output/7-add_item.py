9dea97f1046b6efa19ad9ac5c44d3068cecd7e78
HoussineTabi <0620036304ht@gmail.com>
2023-09-13 07:13:38 +0000

    task5

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,17 @@
 """this place ofcomment that describes my models"""
 import json
 import sys
+import os.path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+filename = "add_item.json"
+my_list = []
 
+if os.path.exists(filename) and os.path.getsize(filename) > 0:
+    my_list = load_from_json_file(filename)
 
-arguments = sys.argv[1:]
-save_to_json_file(arguments, "add_item.json")
-load_from_json_file("add_item.json")
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, filename)
