df76aa7cd608b0d14589a01135764f49653a21f1
HoussineTabi <0620036304ht@gmail.com>
2023-09-13 00:14:17 +0000

    task5

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,17 +4,23 @@ this module of the class student
 """
 
 
-class Stduent:
+class Student:
     """
     the student class
     """
 
     def __init__(self, first_name, last_name, age):
+        """
+        instantiation method
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """
+        to_json method
+        """
         return self.__dict__
 
 
