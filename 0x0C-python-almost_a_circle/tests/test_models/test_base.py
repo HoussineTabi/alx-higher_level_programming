b6433a478660f750bb312250f49ba3923822865c
HoussineTabi <0620036304ht@gmail.com>
2023-09-18 18:44:57 +0000

    tasks

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+The module test of the base with framework unittest
+"""
+import sys
+import unittest
+path = "/home/vagrant/alx/alx-higher_level_programming"
+path += "/0x0C-python-almost_a_circle/models"
+sys.path.append(path)
+from base import Base
+
+
+class testBase(unittest.TestCase):
+    """
+    the class test of the Base class
+    """
+
+    def setUp(self):
+        self.b1 = Base()
+        self.b2 = Base()
+
+    def test_id(self):
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 2)
+        self.b3 = Base(12)
+        self.assertEqual(self.b3.id, 12)
+        self.b4 = Base()
+        self.assertEqual(self.b4.id, 4)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
