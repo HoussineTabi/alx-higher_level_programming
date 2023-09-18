2f3c7388e9c765bac0370c1e606bfc3f2953466b
HoussineTabi <0620036304ht@gmail.com>
2023-09-18 19:17:12 +0000

    tasks

--- 0x0C-python-almost_a_circle/tests/README.md
+++
@@ -1,1 +0,0 @@
-the faile of tests
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+the test module of the square
+"""
+import unittest
+import sys
+
+
+class testSquare(unittest.TestCase):
+    """
+    the square test class
+    """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
