603f6b3f74066be2b6e5a01b5b0fedf08c5d713a
HoussineTabi <0620036304ht@gmail.com>
2023-10-17 16:54:18 +0000

    task_1

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- convert hbtn_0c_0 to UTF8 in MYsql
+-- Converts the entire database hbtn_0c_0 to UTF8.
 USE `hbtn_0c_0`
-ALTER TABLE `firs_table`
+ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
