2755b0c0bbe532247f5478c839b6907d756ce9c3
HoussineTabi <0620036304ht@gmail.com>
2023-10-17 16:25:08 +0000

    task_7

---
+++ 0x0D-SQL_introduction/13-chnge_class.sql
@@ -0,0 +1,3 @@
+-- Deletes element when score equal a specefeid value
+DELETE FROM `second_table`
+WHERE `score` <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- Computes the score of all records in the table
+SELECTE AVG(`score`) AS `average` FROM `second_table`;
