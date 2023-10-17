942e07e079e9a661da9c143bd373dbfb1a52d013
HoussineTabi <0620036304ht@gmail.com>
2023-10-17 15:40:04 +0000

    task_7

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,6 @@
 -- lists all scores that greater than or equal 10
-SELECT `score`, `name` FROM `second_table` WHERE `socre` >= 10 ORDER BY `score` DESC;
+-- ordered by descending score
+SELECT `score`, `name` 
+FROM `second_table` 
+WHERE `socre` >= 10 
+ORDER BY `score` DESC;
