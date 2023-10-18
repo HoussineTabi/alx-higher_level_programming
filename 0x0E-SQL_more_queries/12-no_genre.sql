a67dfba6cc9050097448d9261566c4952a5758ae
HoussineTabi <0620036304ht@gmail.com>
2023-10-18 20:52:36 +0000

    task7

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -6,4 +6,4 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show_genres.show_genres
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_show.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
