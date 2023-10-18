81c85ede28d018914b6767ce4c3f70587312b262
HoussineTabi <0620036304ht@gmail.com>
2023-10-18 14:20:31 +0000

    task2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,9 @@
 -- Creates database hbtn_0d_2 and the user user_0d_2
+-- The user_2 most have privileges just to hbtn
+
 CREATE DATABASE NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* 
+TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
