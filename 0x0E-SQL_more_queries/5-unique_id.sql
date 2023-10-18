d1a18b56b509d4cb1e554b675d9633ce966b4dde
HoussineTabi <0620036304ht@gmail.com>
2023-10-18 14:50:37 +0000

    task5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- creates the table unique_id on Mysql server
 
-create table if not exists unique_di
+create table if not exists unique_id
 (id int not null default 1 unique, name varchar(256) not null);
