042231c24f1fb503bc83ed6f021f98beecf79c4c
HoussineTabi <0620036304ht@gmail.com>
2023-10-18 14:02:43 +0000

    task0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Script that creates a new user user_0d_1.
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
