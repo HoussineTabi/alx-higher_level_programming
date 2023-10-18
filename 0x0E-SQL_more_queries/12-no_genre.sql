132ece6199185ebd815752d091d50878dce4a8ea
HoussineTabi <0620036304ht@gmail.com>
2023-10-18 20:50:45 +0000

    task7

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,9 @@
 -- Lists all shows contained in hbtn_0d_tvshow using only SELECT command
 -- The database name will be passed as an argument
 
-select tv_shows.title, tv_show_genres.genre_id
-from tv_shows
-left join tv_show_genres
-on tv_show_genres.show_genres
-where tv_show_genres.show_id is NULL
-order by tv_show.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_genres
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_show.title, tv_show_genres.genre_id;
