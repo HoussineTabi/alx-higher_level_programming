850ac0e3dbde4233fa7ea080b26f7c9fe25e5064
HoussineTabi <0620036304ht@gmail.com>
2023-10-18 16:01:53 +0000

    task7

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have one gener linked.
 -- Display all recoreds sorted
 
-select tv_shows.title, tv_show_genres.genre_id
-from tv_shows
-inner join tv_show_genres
-on tv_shows.id=tv_show_genres.show_id
-order by tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
