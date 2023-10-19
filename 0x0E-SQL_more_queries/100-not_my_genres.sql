0fe21e2311c81ad0877de9bb672bbde4849dac3b
HoussineTabi <0620036304ht@gmail.com>
2023-10-19 08:25:16 +0000

    task13

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,6 @@
 -- Converts the entire database hbtn_0c_0 to UTF8.
-USE `hbtn_0c_0`
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SELECT tg.name
+FROM tv_genres tg
+LEFT JOIN tv_shows ts ON ts.genre_id = tg.id
+WHERE ts.title IS NULL OR ts.title <> 'Dexter'
+ORDER BY tg.name;
