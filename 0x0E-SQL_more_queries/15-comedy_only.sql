c9f3b10a34214103fa895d6d779675e05543b9f7
HoussineTabi <0620036304ht@gmail.com>
2023-10-19 08:22:22 +0000

    task13

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,11 @@
--- lists all comedy shows in hbtn_0d_tv_shows.
--- records are ordered in a descending order show title
-
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
+-- Records are ordered by descending show title.
 SELECT t.`title`
-FROM `tv_shows` AS t
-INNER `tv_show_genres` AS s
-ON t.`id` = s.`show_id`
-INNER JOIN `tv_genre` AS g
-ON g.`id` = s.`genre_id`
-WHERE g.`name` = "Comedy"
-ORDER BY t.`title`;
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
