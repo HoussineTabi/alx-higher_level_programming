3ad1c326a9e159d27047d216e4969122ac2e7873
HoussineTabi <0620036304ht@gmail.com>
2023-11-13 21:16:06 +0000

    task6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * script that prints three lines c is fun python is cool 
+ * javascript is amazing
+ */
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i;
+for (i = 0; i < 3; i++)
+{
+	console.log(myArray[i]);
+}
