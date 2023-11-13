41ca2861d6592b62f7e6a87b1586771d728b4434
HoussineTabi <0620036304ht@gmail.com>
2023-11-13 22:01:43 +0000

    task12

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Script that searches for the second biggest of a list
+
+all the arguments can be converted to integers
+*/
+const myObject = {
+	type: 'object',
+	value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
