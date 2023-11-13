ca354ca5f41712432681a6f0aedd6261d3cdfdc0
HoussineTabi <0620036304ht@gmail.com>
2023-11-13 21:40:19 +0000

    task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/*
+Script that prints x times the string "C is fun"
+*/
+
+if (process.argv[2] === null || isNaN(parseInt(process.argv[2]))) {
+	console.log('Missing number of occurences')
+}
+else {
+	let i, j;
+	let nb = parseInt(process.argv[2]);
+	for (i = 0; i < nb; i++)
+		{
+			let square = '';
+			for (j = 0; j < nb; j++)
+			{
+				square +=  'x';
+			}
+			console.log(square);
+		}
+}
