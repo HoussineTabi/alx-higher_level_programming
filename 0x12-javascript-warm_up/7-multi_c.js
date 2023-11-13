2aa08d9b46291dd80cb8d43ea2dc70270cdd9fbf
HoussineTabi <0620036304ht@gmail.com>
2023-11-13 21:29:47 +0000

    task7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+Script that prints x times the string "C is fun"
+*/
+
+if (process.argv[2] === null || isNaN(parseInt(process.argv[2]))) {
+	console.log('Missing number of occurences')
+}
+else {
+	let i;
+	for (i = 0; i < parseInt(process.argv[2]); i++)
+		{
+			console.log('C is fun');
+		}
+}
