815b85bbd5f3eda2fdcf0af8d957e8c50e731acc
HoussineTabi <0620036304ht@gmail.com>
2023-11-13 20:05:24 +0000

    tasks

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/*
+ * this script will print javascript on the screen
+ */
+let myVar = "JavaScript is amazing";
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/*
+ * pritns the three lines with some data
+ */
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
