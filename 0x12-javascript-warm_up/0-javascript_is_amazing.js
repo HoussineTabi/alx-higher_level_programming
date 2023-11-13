778e496e00de1fc1f00f4e9cf16058930d96c9b7
HoussineTabi <0620036304ht@gmail.com>
2023-11-13 20:18:33 +0000

    tasks_option

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
- * this script will print javascript on the screen
- */
+this script will print javascript on the screen
+*/
 const myVar = "JavaScript is amazing";
 console.log(myVar);
