fbfdb70e1d9b1191598b1a0a18a67ecd462cea7d
HoussineTabi <0620036304ht@gmail.com>
2023-11-14 14:46:14 +0000

    task

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,21 +2,21 @@
 // This class describe a rectangle
 
 class Rectangle {
-	constructor(w, h) {
-		if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0)
-		{
-		} else {
-			this.width = w;
-			this.height = h;
-		}
-	}
-	print() {
-		let row = '';
-		let i;
-		for (i = 0; i < this.width; i++)
-			row += 'X';
-		for (i = 0; i < this.height; i++)
-			console.log(row);
-	}
+  constructor(w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0)
+    {
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let row = '';
+    let i;
+    for (i = 0; i < this.width; i++)
+      row += 'X';
+    for (i = 0; i < this.height; i++)
+      console.log(row);
+  }
 }
 module.exports = Rectangle;
