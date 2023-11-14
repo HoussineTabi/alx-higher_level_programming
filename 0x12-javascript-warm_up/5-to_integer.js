55b8b1f4f2a1e607adf20808afa8ced56ab5bb8b
HoussineTabi <0620036304ht@gmail.com>
2023-11-14 14:03:00 +0000

    tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ if the argument the argument can be converted to an integer
 */
 
 if (isNaN(parseInt(process.argv[2]))) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -8,5 +8,5 @@ const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < 3; i++)
 {
-	console.log(myArray[i]);
+  console.log(myArray[i]);
 }
