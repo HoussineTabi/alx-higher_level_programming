df6f6133bfdbb352c23f072a1344f9c783f20192
HoussineTabi <0620036304ht@gmail.com>
2023-11-14 14:19:20 +0000

    tasks

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -5,8 +5,8 @@ Script that searches for the second biggest of a list
 all the arguments can be converted to integers
 */
 const myObject = {
-	type: 'object',
-	value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 myObject.value = 89;
