e7590ab53858224c1252d655a66674edba7b4d41
HoussineTabi <0620036304ht@gmail.com>
2023-11-14 14:17:15 +0000

    tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,11 @@
 /*
 Script that prints x times the string "C is fun"
 */
-
 if (process.argv[2] === null || isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurences')
 } else {
   let i;
   for (i = 0; i < parseInt(process.argv[2]); i++) {
-      console.log('C is fun');
+    console.log('C is fun');
   }
 }
