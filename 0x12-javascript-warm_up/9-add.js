f2b4e2f2325374b35badcb0146d928164b204a71
HoussineTabi <0620036304ht@gmail.com>
2023-11-14 14:14:37 +0000

    tasks

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,11 +2,11 @@
 /*
 script that prints the addition of two integer
 */
-function add(a , b) {
+ function add(a, b) {
    return a + b;
-}
+ }
 if (isNaN(parseInt(process.argv[2]))|| isNaN(parseInt(process.argv[3]))) {
   console.log('NaN');
 } else {
-  console.log(add(parseInt(process.argv[2]),parseInt(process.argv[3])));
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
 }
