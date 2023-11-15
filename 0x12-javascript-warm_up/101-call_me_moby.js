637676e36dea49fc4bef2f77b0c641cae7e77a05
HoussineTabi <0620036304ht@gmail.com>
2023-11-15 13:34:28 +0000

    tasks_op

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,7 @@
-#!/usr/bin/ndoe
-// execut x times a function.
+#!/usr/bin/node
+
 exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++)
-	theFunction();
+  for (let index = 0; index < x; index++) {
+    theFunction();
+  }
 };
