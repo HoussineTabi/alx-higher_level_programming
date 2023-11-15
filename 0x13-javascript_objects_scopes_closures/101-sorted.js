5e75e91af4f51435238b6d8b6c3500849bfdacdd
HoussineTabi <0620036304ht@gmail.com>
2023-11-15 12:05:06 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,13 +2,13 @@
 /*
 Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
 */
-const data_dict = require('./101-data').dict;
+const dict = require('./101-data').dict;
 const newDict = {};
 
-for (const key in data_dict) {
-  if (typeof (newDict[data_dict[key]]) === 'undefined') {
-    newDict[data_dict[key]] = [];
+for (const key in dict) {
+  if (typeof (newDict[dict[key]]) === 'undefined') {
+    newDict[dict[key]] = [];
   }
-  newDict[data_dict[key]].push(key);
+  newDict[dict[key]].push(key);
 }
 console.log(newDict);
