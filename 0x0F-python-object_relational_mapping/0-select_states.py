3b17a68cff990b3530d801237d31f33b3f55e817
HoussineTabi <0620036304ht@gmail.com>
2023-11-18 17:39:08 +0000

    tasks

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+script that lists all the states from the hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 4):
+        exit();
+    db = MySQLdb.connect(host = "localhost",user=sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
+    cur = db.cursor();
+    cur.execute("SELECT * FROM states")
+    result = cur.fetchall()
+    for row in result:
+        print(row)
