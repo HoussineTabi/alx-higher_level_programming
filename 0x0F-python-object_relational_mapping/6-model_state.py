749103225d88b86673c32929c6523b0e5bd80435
HoussineTabi <0620036304ht@gmail.com>
2023-11-19 12:12:44 +0000

    tasks_added

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -10,7 +10,8 @@ from sqlalchemy import (create_engine)
 
 
 if __name__ == "__main__":
+    arg1 = argv[1], arg2 = argv[2], arg3 = argv[3]
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(arg1, arg2, arg3),
             pool_pre_ping=True)
-    Base.metadata.create_all(engine)            
+    Base.metadata.create_all(engine)
