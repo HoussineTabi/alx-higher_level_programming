9ba6a82d04ad78cad1629692a89a88a0c4ace9f5
HoussineTabi <0620036304ht@gmail.com>
2023-11-30 13:49:19 +0000

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # bash script that takes in a URL and sends a request to that URL
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
