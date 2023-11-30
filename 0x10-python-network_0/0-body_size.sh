7667f9962b6502cbd9d0a0e365d4c23159dd877f
HoussineTabi <0620036304ht@gmail.com>
2023-11-30 13:44:07 +0000

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # bash script that takes in a URL and sends a request to that URL
-response=$(curl -s -w "%{size_download}" -o /dev/null "$1")
-echo "$response"
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
