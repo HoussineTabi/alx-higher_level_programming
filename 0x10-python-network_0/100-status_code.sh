55cb4b21726cd514cabdd5610ed99e25c4352586
HoussineTabi <0620036304ht@gmail.com>
2023-11-30 16:58:39 +0000

    tasks

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a request to a url that passed as an argument
-curl -s -o /dev/null -w '%(http_code)' "$1"
+# This program sends a request to a URL passed as an argument, and displays only the status code of the response.
+curl -s -o /dev/null -w '%{http_code}' "$1"
