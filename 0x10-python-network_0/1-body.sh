8941e3679782fdba5d5502722610f8344ff697b3
HoussineTabi <0620036304ht@gmail.com>
2023-11-30 14:00:36 +0000

    task1

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# script that sends a GET request to the url and displays the body
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
