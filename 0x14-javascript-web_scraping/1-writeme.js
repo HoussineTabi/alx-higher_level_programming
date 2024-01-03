92109dc6513bd0887718c18aea3deaea4ae684c4
HoussineTabi <0620036304ht@gmail.com>
2024-01-03 21:59:43 +0000

    task_1_write_into_file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+Write a script that writes a string to a file.
+
+The first argument is the file path
+The second argument is the string to write
+The content of the file must be written in utf-8
+*/
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+    return (-1);
+  }
+});
