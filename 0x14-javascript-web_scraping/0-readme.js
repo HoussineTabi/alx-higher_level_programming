0a01c7c45294321ff933f4c8e93daa910f3074ec
HoussineTabi <0620036304ht@gmail.com>
2024-01-03 21:47:42 +0000

    task_0_read_and_prints_the_content_of_a_file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 /*
-Write a script to read and print the content of a file.
-
+Write a script that reads and prints the content of a file.
 
 The first argument is the file path
-The content of the file must be read with utf-8
-If an error occurred during the reading, print error object
+The content of the file must be read in utf-8
+If an error occurred during the reading, print the error object
 */
 const fs = require('fs');
 
